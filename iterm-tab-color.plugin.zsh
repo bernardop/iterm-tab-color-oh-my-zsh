af7d4ef9fb0318d3e8617fa7265b44a766f2a823
Bernardo Pacheco <berna.ipach@gmail.com>
2018-10-23 17:28:00 -0400

    Initial version of changing colors based on directory

--- iterm-tab-color.plugin.zsh
+++ iterm-tab-color.plugin.zsh
@@ -1,29 +1,55 @@
-iterm-tab-color() {
-	if [ $# -eq 1 ]; then
-		if ( [[ $1 == \#* ]] ); then
-			RED_HEX=${1:1:2}
-			GREEN_HEX=${1:3:2}
-			BLUE_HEX=${1:5:2}
-		else
-			RED_HEX=${1:0:2}
-			GREEN_HEX=${1:2:2}
-			BLUE_HEX=${1:4:2}
-		fi
+tcDirsFilePath="$(dirname "$0")/.tc-dirs"
+declare -A directoryColors
+IFS="="
+while read -r dirPath hexValue
+do
+  if ! ( [[ $dirPath == \#* ]]);then
+  	directoryColors[$dirPath]+=$hexValue
+  fi
+done < $tcDirsFilePath
 
-		RED=$(( 16#${RED_HEX} ))
-		GREEN=$(( 16#${GREEN_HEX} ))
-		BLUE=$(( 16#${BLUE_HEX} ))
-
-		echo -ne "\033]6;1;bg;red;brightness;$RED\a"
-    	echo -ne "\033]6;1;bg;green;brightness;$GREEN\a"
-    	echo -ne "\033]6;1;bg;blue;brightness;$BLUE\a"
-
-    	return 1
-	fi
-
-	echo -ne "\033]6;1;bg;red;brightness;$1\a"
-    echo -ne "\033]6;1;bg;green;brightness;$2\a"
-    echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+function directory_tab_color() {
+  if [ ! -z "${directoryColors[${PWD}]}" ];then
+    iterm_tab_color "${directoryColors[${PWD}]}"
+  else
+    iterm_tab_color
+  fi
 }
 
-alias tc='iterm-tab-color'
+function iterm_tab_color() {
+  if [ $# -eq 0 ]; then
+    # Reset tab color if called with no arguments
+	echo -ne "\033]6;1;bg;*;default\a"
+	return 1
+  elif [ $# -eq 1 ]; then
+    if ( [[ $1 == \#* ]] ); then
+	  # If single argument starts with '#', skip first character to find hex value
+	  RED_HEX=${1:1:2}
+	  GREEN_HEX=${1:3:2}
+	  BLUE_HEX=${1:5:2}
+	else
+	  # If single argument doesn't start with '#', assume it's hex value
+	  RED_HEX=${1:0:2}
+	  GREEN_HEX=${1:2:2}
+	  BLUE_HEX=${1:4:2}
+	fi
+
+	RED=$(( 16#${RED_HEX} ))
+	GREEN=$(( 16#${GREEN_HEX} ))
+	BLUE=$(( 16#${BLUE_HEX} ))
+
+	echo -ne "\033]6;1;bg;red;brightness;$RED\a"
+	echo -ne "\033]6;1;bg;green;brightness;$GREEN\a"
+	echo -ne "\033]6;1;bg;blue;brightness;$BLUE\a"
+
+	return 1
+  fi
+
+  # If more than 1 argument, assume 3 arguments were passed
+  echo -ne "\033]6;1;bg;red;brightness;$1\a"
+  echo -ne "\033]6;1;bg;green;brightness;$2\a"
+  echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+}
+
+alias tc='iterm_tab_color'
+chpwd_functions=(${chpwd_functions[@]} "directory_tab_color")
